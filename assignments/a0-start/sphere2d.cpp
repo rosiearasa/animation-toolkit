0500fa8c0f80846eca3b92fe8939c1861ae346d5
rosiearasa <rosearasa7522@gmail.com>
2021-09-08 12:23:38 -0400

    A

--- assignments/a0-start/sphere2d.cpp
+++ assignments/a0-start/sphere2d.cpp
@@ -9,13 +9,24 @@ class Sphere2D : public atkui::Framework {
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
+    setColor(vec3(1,0,0));
+    //drawing a red sphere behind the green sphere
+    double x1 = width() * 0.5;
+    double y1 = height() * 0.5;
+    double radius1 = 200;
+    
+    drawSphere(vec3(x1,y1,-1000), radius1);
+
     setColor(vec3(0,1,0));
 
-    // draw a sphere at center of screen
+    //draw a sphere at center of screen
     double x = width() * 0.5;
     double y = height() * 0.5;
     double radius = 100; 
+
     drawSphere(vec3(x,y,0), radius);
+
+  
   }
 };
 
