d58034ec77712c4dac8a08e8e7632381286bfe01
rosearasa7522@gmail.com <rosearasa7522@gmail.com>
2021-09-09 14:59:14 -0400

    screenshot1

--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -1,29 +1,57 @@
 #include "atkui/framework.h"
+#include <random>
 
 using glm::vec3;
-
+vec3 currentPosition;
+vec3 vel;
 class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
   }
 
-  virtual void scene() {
+void scene() {
+    
     // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
 
     // draw a sphere at center of the world
     float radius = 50.0;
     drawSphere(vec3(0), radius);
-
-
-
-    
+  
   }
+
+
+  //the initial position of the sphere
+void setup(){
+    currentPosition = vec3(0,0,0);
+    vel = vec3(agl::randomUnitVector());
+  }
+
+
+
+  //update
+ void update(){
+  currentPosition = currentPosition + vel * dt();
+  }
+
+
+void keyUp(int key, int mods){
+  if ( key == GLFW_KEY_K){
+    //reset position of object to (0,0,0)
+    currentPosition = vec3(0,0,0);
+    }
+    if (key == GLFW_KEY_SPACE){
+      update();
+    }
+  
+ }
+
 };
 
 int main(int argc, char** argv)
 {
   Sphere3D viewer;
-  viewer.run();
+  
+  viewer.run();//this runs the game loop
   return 0;
 }
