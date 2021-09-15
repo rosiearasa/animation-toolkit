204a9b84d9c89d9f5e991db22b124d4ce11f5b58
rosearasa7522@gmail.com <rosearasa7522@gmail.com>
2021-09-15 13:07:43 -0400

    buggy eyes

--- assignments/a1-hello/eyes.cpp
+++ assignments/a1-hello/eyes.cpp
@@ -6,44 +6,38 @@ class Eyes : public atkui::Framework
   public:
     Eyes() : atkui::Framework(atkui::Orthographic) {
     }
-    virtual void setup(){
-      theta = 0.0;
-      thetaRate = 0.9;
-      sR = 50;
-
-    }
-
-    virtual void scene() {
-      //draw two circles  the center
-      setColor (vec3(1,1,1));
   
-      double radius = 100;
+    virtual void scene() {
 
-      double x = width()*0.75;
-      double y= height()/2.0;
-      drawSphere(vec3(x,y,0), radius);
+    setColor(vec3(1,1,1));
+    double x = width() * 0.5;
+    double y = height() * 0.5;
+    double radius = 100; 
+    double s1X =x *0.7;
+    double s2X = x * 1.5;
+    drawSphere(vec3(s1X,y,-1000), radius);
+    drawSphere(vec3(s2X,y,-1000), radius);
 
-      double x1 = width()*0.25;
-      double y1= height()/2.0;
-      drawSphere(vec3(x1,y1,0), radius);
+    smallSphere(s1X ,radius);
+    smallSphere(s2X ,radius);
 
 
-      setColor(vec3(1,0,0));
-      smallSphere();
-    
-
-    }
-    virtual void smallSphere(){
-      theta+= thetaRate *dt();
-      float px = 5.0+sR*cos(theta)+ 0.5* width();
-      float py =  sR + 0.5 *height();
-      drawSphere(vec3(px,py,-1000), 80);
 
     }
-private:
-float theta;
-float thetaRate;
-float sR;
+      
+    virtual void smallSphere(float center, float startPos) {
+
+    setColor(vec3(0,0,1));
+    double x = width() * 0.5 *sin(elapsedTime())+center;
+    double y = height() * 0.5;
+    double radius = 30; 
+
+    drawSphere(vec3(x,y,0), radius);
+
+
+
+    }
+  
     
 };
 
