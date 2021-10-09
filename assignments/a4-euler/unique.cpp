f8afaa99024eeffa898a608b78d2f169e701ae63
rosiearasa <rosearasa7522@gmail.com>
2021-10-09 17:37:12 -0400

    readme files

--- assignments/a4-euler/unique.cpp
+++ assignments/a4-euler/unique.cpp
@@ -1,9 +1,32 @@
 #include "atkui/framework.h"
+
 using namespace glm;
 
+class Unique : public atkui::Framework
+{
+  public:
+    Unique() : atkui::Framework(atkui::Orthographic) {
+    }
+    virtual void setup() {
+      
+        
+    }
+    virtual void scene() {
 
+
+
+    }
+
+
+
+private:
+
+
+
+};
 
 int main(int argc, char** argv) {
-   return 0;
+ Unique viewer;
+  viewer.run();
+  return 0;
 }
-
