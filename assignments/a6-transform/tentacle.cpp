71b3a61f9a0deeadb9bc3705d9596be9ad0e9c61
rosiearasa <rosearasa7522@gmail.com>
2021-11-02 15:11:15 -0400

    tentacles

--- assignments/a6-transform/tentacle.cpp
+++ assignments/a6-transform/tentacle.cpp
@@ -1,25 +1,44 @@
 #include "atkui/framework.h"
 #include "atk/toolkit.h"
+
 using namespace atk;
 using glm::vec3;
 
-class Tentacle : public atkui::Framework {
+class Tentacle : public atkui::Framework
+{
 public:
    Tentacle() : atkui::Framework(atkui::Perspective) {}
    virtual ~Tentacle() {}
 
-   virtual void setup() {
+   virtual void setup()
+   {
       lookAt(vec3(200), vec3(0));
 
-      Joint* root  = new Joint("root");
-      root->setLocalTranslation(vec3(0,0,0));
+      Joint *root = new Joint("root");
+      root->setLocalTranslation(vec3(0, 0, 0));
       _tentacle.addJoint(root);
 
-      Joint* joint1  = new Joint("joint1");
-      joint1->setLocalTranslation(vec3(0,50,0));
+      Joint *joint1 = new Joint("joint1");
+      joint1->setLocalTranslation(vec3(0, 50, 0));
       _tentacle.addJoint(joint1, root);
 
       // todo: add more joints
+      Joint *joint2 = new Joint("joint2");
+      joint2->setLocalTranslation(vec3(0, 50, 0));
+      _tentacle.addJoint(joint2, joint1);
+
+      Joint *joint3 = new Joint("joint3");
+      joint3->setLocalTranslation(vec3(0, 50, 0));
+      _tentacle.addJoint(joint3, joint2);
+
+      Joint *joint4 = new Joint("joint4");
+      joint4->setLocalTranslation(vec3(0, 50, 0));
+      _tentacle.addJoint(joint4, joint3);
+
+      Joint *joint5 = new Joint("joint5");
+      joint5->setLocalTranslation(vec3(0, 50, 0));
+      _tentacle.addJoint(joint5, joint4);
+
       _tentacle.fk(); // compute local2global transforms
    }
 
@@ -28,23 +47,33 @@ public:
       // todo animate joints
       // hint: use glm::angleAxis(...) to compute quaternions for each joint
       _tentacle.fk(); // computes local2global transforms
-      setColor(vec3(0,1,0));
+      setColor(vec3(0, 1, 0));
 
-      // todo: loop over all joints and draw
-      Joint* parent = _tentacle.getByID(0);
-      Joint* child = _tentacle.getByID(1);
-      vec3 globalParentPos = parent->getGlobalTranslation();
-      vec3 globalPos = child->getGlobalTranslation();
-      drawEllipsoid(globalParentPos, globalPos, 5);
+      float angle = sin(elapsedTime() / 5.0f); //sine to compute rotation
+      for (int i = 0; i < 5; i++)
+      {
+         glm::quat Q = glm::angleAxis((angle * i), vec3(0, 0, 1));
+         Joint *joint0 = _tentacle.getByID(i);
+         joint0->setLocalRotation(Q);
+      }
+      for (int i = 0; i < 5; i++)
+      {
+
+         // todo: loop over all joints and draw
+         Joint *parent = _tentacle.getByID(i);
+         Joint *child = _tentacle.getByID(i + 1);
+         vec3 globalParentPos = parent->getGlobalTranslation();
+         vec3 globalPos = child->getGlobalTranslation();
+         drawEllipsoid(globalParentPos, globalPos, 5);
+      }
    }
 
 protected:
    Skeleton _tentacle;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
    Tentacle viewer;
    viewer.run();
-} 
-
+}
