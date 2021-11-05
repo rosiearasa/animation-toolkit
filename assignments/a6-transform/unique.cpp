6ab6237e22d6746c5b5b6e4d03149a1c2d819554
rosiearasa <rosearasa7522@gmail.com>
2021-11-05 17:51:15 -0400

    unique

--- assignments/a6-transform/unique.cpp
+++ assignments/a6-transform/unique.cpp
@@ -35,9 +35,19 @@ public:
       joint4->setLocalTranslation(vec3(0, 50, 0));
       _tentacle.addJoint(joint4, joint3);
 
+    Joint *joint40 = new Joint("joint40");
+      joint40->setLocalTranslation(vec3(0, 0, 50));
+      _tentacle.addJoint(joint40, joint4);
+
       Joint *joint5 = new Joint("joint5");
       joint5->setLocalTranslation(vec3(0, 50, 0));
-      _tentacle.addJoint(joint5, joint4);
+      _tentacle.addJoint(joint5, joint3);
+
+      Joint *joint41 = new Joint("joint41");
+      joint41->setLocalTranslation(vec3(50, 50, 0));
+      _tentacle.addJoint(joint41, joint4);
+
+    
 
       _tentacle.fk(); // compute local2global transforms
    }
@@ -49,14 +59,14 @@ public:
       _tentacle.fk(); // computes local2global transforms
       setColor(vec3(0, 1, 0));
 
-      float angle = sin(elapsedTime() / 5.0f); //sine to compute rotation
-      for (int i = 0; i < 5; i++)
+      float angle = sin(elapsedTime() / 2.0f); //sine to compute rotation
+      for (int i = 3; i < 7; i++)
       {
-         glm::quat Q = glm::angleAxis((angle), vec3(0, 0, 1));
+         glm::quat Q = glm::angleAxis((angle*i ), vec3(1, 0, 1));
          Joint *joint0 = _tentacle.getByID(i);
          joint0->setLocalRotation(Q);
       }
-      for (int i = 0; i < 5; i++)
+      for (int i = 0; i < 7; i++)
       {
 
          // todo: loop over all joints and draw
@@ -64,6 +74,7 @@ public:
          Joint *child = _tentacle.getByID(i + 1);
          vec3 globalParentPos = parent->getGlobalTranslation();
          vec3 globalPos = child->getGlobalTranslation();
+
          drawEllipsoid(globalParentPos, globalPos, 5);
       }
    }
